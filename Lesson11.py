db535200482a43b0e7d08bdb2a166daa6cdd45fd
SGrechushkin <sgrechushkin@gmail.com>
2025-04-15 09:44:19 +0300

    Lesson 1 - Intro Lesson 2 - Variables, Data Type Lesson 3 - Python Casting Lesson 4 - Booleans and Operators Lesson 5 - List Lesson 6 - List Comprehension Lesson 7 - Tuples Lesson 8 - Sets Lesson 9 - Dictionaries Lesson10 - If...Else,Match,While loop Lesson11 - For loops Lesson12 - Functions

--- Lesson11.py
+++ Lesson11.py
@@ -1,3 +1,149 @@
 #----------------------------
     #Python For Loops
 #https://www.w3schools.com/python/python_for_loops.asp
+
+#With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+'''
+
+#Looping Through a String
+'''
+for x in "banana":
+  print(x) 
+result:
+b
+a
+n
+a
+n
+a
+'''
+
+#The break Statement
+#With the break statement we can stop the loop before it has looped through all the items:
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+result:
+apple
+banana
+'''
+
+#Exit the loop when x is "banana", but this time the break comes before the print:
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+result:
+apple
+'''
+
+#The continue Statement
+#With the continue statement we can stop the current iteration of the loop, and continue with the next:
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+  
+result:
+apple
+cherry
+'''
+
+#The range() Function
+#To loop through a set of code a specified number of times, we can use the range() function,
+#The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+'''
+for x in range(6):
+  print(x)
+  
+result:
+
+for x in range(6):
+  print(x) 
+0
+1
+2
+3
+4
+5
+'''
+
+#The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6):
+'''
+for x in range(2, 6):
+  print(x)
+result: 
+2
+3
+4
+5
+'''
+
+#The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+'''
+for x in range(2, 30, 3):
+  print(x) 
+result:
+
+2
+5
+8
+11
+14
+17
+20
+23
+26
+29
+'''
+
+#Else in For Loop
+#The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+'''
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+'''
+
+#Nested Loops
+#A nested loop is a loop inside a loop.
+#The "inner loop" will be executed one time for each iteration of the "outer loop":
+'''
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+    
+result:
+red apple
+red banana
+red cherry
+big apple
+big banana
+big cherry
+tasty apple
+tasty banana
+tasty cherry
+'''
+
+#The pass Statement
+#for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+'''
+for x in [0, 1, 2]:
+  pass
+'''
+
---
+++ Lesson12.py
@@ -0,0 +1,99 @@
+#--------------------------
+    #Python Functions
+#A function is a block of code which only runs when it is called.
+#You can pass data, known as parameters, into a function.
+#A function can return data as a result.
+
+#Creating a Function
+#In Python a function is defined using the def keyword:
+'''
+def my_function():
+  print("Hello from a function")
+'''
+
+#Calling a Function
+#To call a function, use the function name followed by parenthesis:
+'''
+def my_function():
+  print("Hello from a function")
+
+my_function()
+'''
+
+#Arguments
+#Information can be passed into functions as arguments.
+#Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+#The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+'''
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+result:
+Emil Refsnes
+Tobias Refsnes
+Linus Refsnes
+'''
+
+#Number of Arguments
+#By default, a function must be called with the correct number of arguments. Meaning that if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+'''
+def my_function(fname, lname):
+  print(fname + " " + lname)
+names = [("Emil", "Refsnes"), ("Anna", "Ivanova"), ("John", "Smith")]
+
+for fname, lname in names:
+    my_function(fname, lname)
+result:
+Emil Refsnes
+Anna Ivanova
+John Smith
+'''
+
+#Arbitrary Arguments, *args
+#If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+#This way the function will receive a tuple of arguments, and can access the items accordingly:
+'''
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+'''
+
+#Keyword Arguments
+#You can also send arguments with the key = value syntax.
+#This way the order of the arguments does not matter.
+'''
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+'''
+
+#Arbitrary Keyword Arguments, **kwargs
+#If you do not know how many keyword arguments that will be passed into your function, add two asterisk: ** before the parameter name in the function definition.
+#This way the function will receive a dictionary of arguments, and can access the items accordingly:
+'''
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+'''
+
+#Default Parameter Value
+#The following example shows how to use a default parameter value.
+#If we call the function without argument, it uses the default value:
+'''
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+'''
+
+#Passing a List as an Argument
