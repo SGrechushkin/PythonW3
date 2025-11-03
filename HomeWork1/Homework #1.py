81655889080845cf05ea45b475bde6be40bb563d
SGrechushkin <sgrechushkin@gmail.com>
2025-11-03 09:57:28 +0200

    Lesson 1 - Intro Lesson 2 - Variables, Data Type Lesson 3 - Python Casting Lesson 4 - Booleans and Operators Lesson 5 - List Lesson 6 - List Comprehension Lesson 7 - Tuples Lesson 8 - Sets Lesson 9 - Dictionaries Lesson10 - If...Else,Match,While loop Lesson11 - For loops Lesson12 - Functions HomeWork #1

---
+++ HomeWork1/Homework #1.py
@@ -0,0 +1,10 @@
+def my_function():
+  print("Enter the first number")
+  num1 = float(input())
+  print("Enter the second number")
+  num2 = float(input())
+  print ("Result of addition: ", num1 + num2)
+  print ("Result of multiplication: ", num1 * num2)
+  print("Result of division: ", num1 / num2)
+
+my_function()
