cbe097bd7b21cd09e415b5652e2da65f0b266b4b
SGrechushkin <sgrechushkin@gmail.com>
2025-11-04 09:07:41 +0200

    Lesson 1 - Intro Lesson 2 - Variables, Data Type Lesson 3 - Python Casting Lesson 4 - Booleans and Operators Lesson 5 - List Lesson 6 - List Comprehension Lesson 7 - Tuples Lesson 8 - Sets Lesson 9 - Dictionaries Lesson10 - If...Else,Match,While loop Lesson11 - For loops Lesson12 - Functions HomeWork1

--- HomeWork1/Homework #1.py
+++ HomeWork1/Homework #1.py
@@ -1,10 +1,18 @@
 def my_function():
+  # Оголошую функцію без заздалегідь заданих аргументів
   print("Enter the first number")
+  # виводимо повідомлення на екран
   num1 = float(input())
+  # запитуємо введення першого числа та перетворюємо його у десяткове
   print("Enter the second number")
-  num2 = float(input())
-  print ("Result of addition: ", num1 + num2)
+  num2 = int(input())
+  # запитуємо введення другого числа та перетворюємо його у ціле
+  print (f"Result of addition: {num1 + num2}")
+  # Використовуємо f-string для виводу суми чисел з текстом. Мені не дуже подобається, бо я про неї забуваю
   print ("Result of multiplication: ", num1 * num2)
-  print("Result of division: ", num1 / num2)
+  # Використовую кому для виводу декілької аргументів через прінт.
+  print("Result of division: ", round(num1 / num2, 3))
+  # Округлюю значення після ділення і залишаю 3 знаки після коми.
 
 my_function()
+# Викликаю функцію.
